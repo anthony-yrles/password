0aee198271766b81ad5cc0002e63c59536d2b15d
Sluchai40 <mabbolos8@gmail.com>
2023-12-11 13:22:11 +0100

    End of the password

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from password import password_input
+from random_choice import *
 from hash import hashing
 from password_save import save_password
 
-while True:
-    password = password_input()
-    password = hashing(password)
-    print(password)
-    save_password(password)
+"""
+from password import password_input
+La premiere partie de l'excercice, la création avec un input est dans le fichier password.py
+"""
+
+total_random = random_total()
+password = random_end(total_random)
+password_hashed = hashing(password)
+save_password(password_hashed)
+print(f"Le mot de passe généré est {password} et son hachage est {password_hashed}")
--- password_save.py
+++ password_save.py
@@ -6,6 +6,15 @@ def save_password(password):
             passwords = json.load(password_list)
     except (FileNotFoundError, json.decoder.JSONDecodeError):
         passwords = []
+    password_check(password, passwords)
     passwords.append(password)
     with open("password_file.json", "w") as password_list:
         json.dump(passwords, password_list)
+
+
+def password_check(password, passwords):
+    while True:
+        for i in passwords:
+            if i == password:
+                password = input("Ce mot de passe existe déjà veuillez en saisir un nouveau : ")
+        break
